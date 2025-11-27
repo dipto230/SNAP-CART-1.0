c2dec690f388916191fb5724d25af4b6a4659919
dipto230 <diptoroy230305@gmail.com>
2025-11-28 03:51:55 +0530

    welcome component section done

---
+++ src/next-auth.d.ts
@@ -0,0 +1,30 @@
+import NextAuth from "next-auth"
+
+declare module "next-auth" {
+  interface User {
+    id: string
+    name: string
+    email: string
+    role: string
+  }
+
+  interface Session {
+    user: {
+      id: string
+      name: string
+      email: string
+      role: string
+    }
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string
+    name: string
+    email: string
+    role: string
+  }
+}
+
+export {}

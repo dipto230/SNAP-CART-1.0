38799adcaf91e3a3ba1125624f7f11a268a49c7f
dipto230 <diptoroy230305@gmail.com>
2025-12-18 23:27:49 +0530

    order card section done

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -184,7 +184,7 @@ const sideBar = menuOpen
 
                   </div>
                 </div>
-                {user.role=="user" &&  <Link href={""} className='flex items-center gap-2 px-3 hover:bg-green-50 rounded-lg text-gray-700 font-medium' onClick={()=>setOpen(false)}>
+                {user.role=="user" &&  <Link href={"/user/my-orders"} className='flex items-center gap-2 px-3 hover:bg-green-50 rounded-lg text-gray-700 font-medium' onClick={()=>setOpen(false)}>
                   <Package className='w-5 h-5 text-green-600' />
                   My Orders
                   
